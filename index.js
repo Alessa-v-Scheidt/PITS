d166d4561000e13f60b6472d83699d46508afb6b
Alessa-v-Scheidt <avscheidt49@gmail.com>
2022-06-17 19:43:30 +0200

    added insecure routes

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const app = express()
   Rate-Limiting
 */
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window`
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window`
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
 // Spezifischer:
@@ -27,20 +27,24 @@ app.use(session({
 }))
 app.use(express.json())
 app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
 
+/*
+  Secure Routes
+*/
+
 app.get('/', (req, res) => {
-  res.redirect('/store/')
+  res.redirect('/secure/')
 })
 
-app.get('/store/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/view/index.html'))
+app.get('/secure/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/view/secureIndex.html'))
 })
 
 
-app.get('/store/purchaseForm', (req, res) => {
+app.get('/secure/purchaseForm', (req, res) => {
   // neues Captcha wird generiert
   const captcha = svgCaptcha.create()
 
@@ -48,18 +52,36 @@ app.get('/store/purchaseForm', (req, res) => {
   req.session.captcha = captcha.text
 
   // Captcha-SVG wird im Template eingefügt
-  res.render(path.join(__dirname + '/view/purchaseForm'), { captcha: captcha.data})
+  res.render(path.join(__dirname + '/view/securePurchaseForm'), { captcha: captcha.data })
 })
 
-app.post('/store/purchaseConfirmation', (req, res) => {
+app.post('/secure/purchaseConfirmation', (req, res) => {
   // Vergleich Daten von Server === Eingabe von Nutzer
-  if(req.session.captcha === req.body.captcha){
-    res.sendFile(path.join(__dirname + '/view/success.html'))
+  if (req.session.captcha === req.body.captcha) {
+    res.sendFile(path.join(__dirname + '/view/secureSuccess.html'))
   } else {
-    res.sendFile(path.join(__dirname + '/view/denied.html'))
-  } 
+    res.sendFile(path.join(__dirname + '/view/secureDenied.html'))
+  }
 })
+
+/*
+  Unsecured Routes
+*/
+app.get('/insecure/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/view/insecureIndex.html'))
+})
+
+
+app.get('/insecure/purchaseForm', (req, res) => {
+  // Captcha-SVG wird im Template eingefügt
+  res.sendFile(path.join(__dirname + '/view/insecurePurchaseForm.html'))
+})
+
+app.post('/insecure/purchaseConfirmation', (req, res) => {
+  res.sendFile(path.join(__dirname + '/view/insecureSuccess.html'))
+})
+
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+  console.log('Server is running on http://localhost:3000')
 })
